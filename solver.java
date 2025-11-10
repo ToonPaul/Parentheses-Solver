f956ee798c9753e9a0e909672361c7d84268a814
Toon Paul <hudallap@gmail.com>
2025-11-10 19:19:50 +0000

    inpro1

--- solver.java
+++ solver.java
@@ -1,10 +1,35 @@
 //Name: Paul Hudalla
 //Date: 11/7/25
-//Determine if all the open parantheses in the string having matching closing parantheses
+//Determine if all the open parantheses in the string have matching closing parantheses
 
 public class Solver{
     public static boolean StringTest(String s){
-        
+        String sub = s.substring(0,1);
+        String sub2;
+        if(sub.equals("(")){
+            for(int i = 1;i < s.length();i++){
+                if(s.indexOf[i].equals(")")){
+                    sub2 = s.substring(i, i+1);
+                }
+            }
+        }
+        else if (sub.equals("[")){
+            for(int i = 1;i < s.length();i++){
+                if(s.indexOf[i].equals("]")){
+                    sub2 = s.substring(i, i+1);
+                }
+            }
+        }
+        else if (sub.equals("{")){
+            for(int i = 1;i < s.length();i++){
+                if(s.indexOf[i].equals("}")){
+                    sub2 = s.substring(i, i+1);
+                }
+            }
+        }
+        else{
+            return false;
+        }
     }
 }
     
