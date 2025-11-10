76d72350a02c52802d63b722f034958d6ff8590b
Toon Paul <hudallap@gmail.com>
2025-11-10 19:05:39 +0000

    sa;ldkjf

--- solver.java
+++ solver.java
@@ -2,28 +2,9 @@
 //Date: 11/7/25
 //Determine if all the open parantheses in the string having matching closing parantheses
 
-public class Generator{
-    String s = "{}";
-    if(s.substring(0,1) == "("){
-        for(int i = 1; i < s.length; i++){
-            if(s.substring(i, i+1) == ")"){
-                
-            }
-        }
-    }
-    if(s.substring(0,1) == "["){
-        for(int i = 1; i < s.length; i++){
-            if(s.substring(i, i+1) == "]"){
-                
-            }
-        }
-    }
-    if(s.substring(0,1) == "{"){
-        for(int i = 1; i < s.length; i++){
-            if(s.substring(i, i+1) == "}"){
-                
-            }
-        }
-
+public class Solver{
+    public static boolean StringTest(String s){
+        
     }
 }
+    
