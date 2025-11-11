c0f7745c4ce2c1cebf1614b7057a5c9d62beae04
Toon Paul <hudallap@gmail.com>
2025-11-11 19:29:22 +0000

    needtotest

--- Attempt2.java
+++ Attempt2.java
@@ -3,10 +3,31 @@
 //Determine if all the open parantheses in the string have matching closing parantheses
 
 public class Attempt2{
-    public static void isValid(String s){
+    public static boolean isValid(String s){
+        for(int i = s.length(); i > 0;i--){
+            if(s.indexOf[i].equals("(")){
+                if(s.indexOf[i+1].equal(")")){
+                    s = s.substring(0,i) + s.substring(i+2);
+                } else{
+                    return false;
+                }
+            } else if (s.indexOf[i].equals("[")){
+                if(s.indexOf[i+1].equal("]")){
+                    s = s.substring(0,i) + s.substring(i+2);
+                } else{
+                    return false;
+                }
+            } else if (s.indexOf[i].equals("{")){
+                if(s.indexOf[i+1].equal("}")){
+                    s = s.substring(0,i) + s.substring(i+2);
+                } else{
+                    return false;
+                }
+            }
         
+        }
     }
     public static void main(String[] args) {
-        
+        System.out.println(isValid("(){}[]"));
     }
 }
