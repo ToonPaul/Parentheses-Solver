7814994e3cc4eb5bb2dcada2b7d5215537e48ca2
Toon Paul <hudallap@gmail.com>
2025-11-12 19:02:05 +0000

    freshstart2

--- Attempt2.java
+++ Attempt2.java
@@ -5,20 +5,20 @@
 public class Attempt2{
     public static boolean isValid(String s){
         for(int i = s.length(); i > 0;i--){
-            if(s.indexOf[i].equals("(")){
-                if(s.indexOf[i+1].equal(")")){
+            if(s.substring(i,i+1).equals("(")){
+                if(s.substring(i+1,i+2).equals(")")){
                     s = s.substring(0,i) + s.substring(i+2);
                 } else{
                     return false;
                 }
-            } else if (s.indexOf[i].equals("[")){
-                if(s.indexOf[i+1].equal("]")){
+            } else if (s.substring(i,i+1).equals("[")){
+                if(s.substring(i+1,i+2).equals("]")){
                     s = s.substring(0,i) + s.substring(i+2);
                 } else{
                     return false;
                 }
-            } else if (s.indexOf[i].equals("{")){
-                if(s.indexOf[i+1].equal("}")){
+            } else if (s.substring(i,i+1).equals("{")){
+                if(s.substring(i+1,i+2).equals("}")){
                     s = s.substring(0,i) + s.substring(i+2);
                 } else{
                     return false;
--- Solver.java
+++ Solver.java
@@ -2,44 +2,9 @@
 //Date: 11/7/25
 //Determine if all the open parantheses in the string have matching closing parantheses
 
-/*public class Solver{
+public class Solver{
     public static boolean StringTest(String s){
-        String sub = s.substring(0,1);
-        String newS = s.substring(1);
-        do{
-        if(sub.equals("(")){
-            for(int i = 1;i < s.length();i++){
-                if(s.indexOf[i].equals(")")){
-                   newS = s.substring(0, i) + s.substring(i + 1); 
-                }
-            }
-        }
-        else{
-            return false;
-        }
-        if (sub.equals("[")){
-            for(int i = 1;i < s.length();i++){
-                if(s.indexOf[i].equals("]")){
-                    newS = s.substring(0, i) + s.substring(i + 1);
-                }
-            }
-        }
-        else{
-            return false;
-        }
-        if (sub.equals("{")){
-            for(int i = 1;i < s.length();i++){
-                if(s.indexOf[i].equals("}")){
-                    newS = s.substring(0, i) + s.substring(i + 1);
-                }
-            }
-        }
-    }while (s.length() > 0);
-        if(s.equal("")){
-            return true;
-        } else{
-            return false;
-        }
+        
     }
     public static void main(String[] args) {
         StringTest("(){}[]");
@@ -53,7 +18,3 @@
     
     }while(s.length() > 0)*/
 
-    /*
-     
-    
-     */
